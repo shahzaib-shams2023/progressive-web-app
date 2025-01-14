4b166e06ca0237f2189243c0c858663f01eb41bc
shahzaib <shahzaib@aptechgdn.net>
2025-01-14 10:19:58 +0500

    Files added

--- sw.js
+++ sw.js
@@ -7,30 +7,32 @@ const urlsToCache = [
 
 // Cache Install
 
-this.addEventListener("install", (event) => {
+// caching.js
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches
-      .open("v1")
-      .then((cache) =>
-        cache.addAll([
-          "/",
-          "/index.html",
-        ]),
-      ),
+    // Open a cache of resources.
+    caches.open(CACHE_NAME).then(cache => {
+      // Begins the process of fetching them. Succeeds only once all
+      // resources have been stored. Even just one failing resource
+      // causes the entire operation to fail.
+      return cache.addAll([
+        "/index.html",
+      ]);
+    })
   );
 });
 
+self.addEventListener("fetch", event => {
+  // No "fetch" events are dispatched to the service worker until it
+  // successfully installs and activates.
 
-// Cache data fetching
-
-self.addEventListener('fetch' , (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            if(response){
-                return response
-            }
-            return fetch(event.request)
-        })
-    )
-})
+  // All operations on caches are async, including matching URLs, so we use
+  // promises heavily. e.respondWith() even takes promises to enable this:
+  event.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    }).catch(() => {
+      return caches.match("/index.html");
+    })
+  );
+});
